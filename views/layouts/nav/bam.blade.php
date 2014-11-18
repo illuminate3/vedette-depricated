88faa0d0dd0572cde0243e88ea9107adc29e3f35
illuminate3 <carsten.sapporo@gmail.com>
2014-11-18 13:49:05 -0600

    bam

--- views/layouts/nav/bam.blade.php
+++ views/layouts/nav/bam.blade.php
@@ -1,18 +1,48 @@
 <ul class="nav navbar-nav">
+
+<li>
+	<a href="{{ route('items.index') }}"><span class="glyphicon glyphicon-wrench"></span> Items</a>
+</li>
+<li>
+	<a href="{{ route('asset.index') }}"><span class="glyphicon glyphicon-wrench"></span> Assets</a>
+</li>
+<li>
+	<a href="{{ route('asset_statuses.index') }}"><span class="glyphicon glyphicon-wrench"></span> asset_statuses</a>
+</li>
+<li>
+	<a href="{{ route('tech_statuses.index') }}"><span class="glyphicon glyphicon-wrench"></span> tech_statuses</a>
+</li>
+<li>
+	<a href="{{ route('rooms.index') }}"><span class="glyphicon glyphicon-wrench"></span> Rooms</a>
+</li>
+
+<li>
+	<a href="{{ route('categories.index') }}"><span class="glyphicon glyphicon-wrench"></span> Manage Categories</a>
+</li>
+
+</ul>
+
+<li class="dropdown">
+	<a class="dropdown-toggle {{ (Request::is('admin*') ? ' active' : '') }}" data-toggle="dropdown" href="#">
+		HR
+		<b class="caret"></b>
+	</a>
+	<ul class="dropdown-menu">
 	<li {{ (strstr(Route::getCurrentRoute()->getPath(),'sites')) ? 'class="active"' : '' }} >
 		{{ link_to('sites', 'Sites') }}
 	</li>
 	<li {{ (strstr(Route::getCurrentRoute()->getPath(),'profiles')) ? 'class="active"' : '' }} >
 		{{ link_to('profiles', 'Staff') }}
 	</li>
-</ul>
+	</ul>
+</li>
 
 @if (Auth::check())
 @if (Auth::user()->hasRoleWithName('Admin'))
 
 <li class="dropdown">
 	<a class="dropdown-toggle {{ (Request::is('admin*') ? ' active' : '') }}" data-toggle="dropdown" href="#">
-		{{ trans('lingos::general.settings') }}
+		HR - {{ trans('lingos::general.settings') }}
 		<b class="caret"></b>
 	</a>
 	<ul class="dropdown-menu">
